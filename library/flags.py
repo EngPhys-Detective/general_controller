bfd8e9045cd56da07cbdf392dd96163973d85e7e
Sasanghasaei <sasangh@student.ubc.ca>
2023-12-04 18:58:18 -0800

    flags

---
+++ library/flags.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+
+class Flags:
+    
+    first_time_red_line_detected = False
+    
+    second_time_red_line_detected = False
+    
+    passed_truck = False
+    
+    reached_roundabout = False
+    
+    drive = True
